c03d5afa0091e4f09c21d972a903ca2d27e44c92
Martim Félix <a100647@alunos.uminho.pt>
2023-12-27 23:35:57 +0000

    Corrigido o IDFS

--- IDFS_Tuplos.py
+++ IDFS_Tuplos.py
@@ -2,24 +2,23 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from queue import Queue
 from collections import OrderedDict
-
-def iterative_deepening_dfs(start, target):
+def iterative_deepening_dfs(graph, start, target):
     visited = set()  # Conjunto para rastrear nós visitados
     path = []  # Caminho para a solução
     order_of_expansion = []  # Ordem de expansão
 
     def dfs(node, depth):
-        if node["value"] in visited:
+        if node in visited:
             return False
-        visited.add(node["value"])
-        order_of_expansion.append(node["value"])
-        if node["value"] == target:
-            path.append(node["value"])
+        visited.add(node)
+        order_of_expansion.append(node)
+        if node == target:
+            path.append(node)
             return True
         if depth > 0:
-            for child in node["children"]:
+            for child in graph.get(node, []):
                 if dfs(child, depth - 1):
-                    path.append(node["value"])
+                    path.append(node)
                     return True
         return False
 
@@ -27,10 +26,9 @@ def iterative_deepening_dfs(start, target):
     while not dfs(start, depth):
         visited.clear()  # Limpar nós visitados para a próxima iteração
         depth += 1
+
     unique_list = list(OrderedDict.fromkeys(order_of_expansion))
-
     return path[::-1], unique_list, depth
-
 graph = {
     ('A',): [('B',), ('C',)],
     ('B',): [('D',), ('E',)],
@@ -49,26 +47,5 @@ graph = {
     ('O',): []
 }
 
-def convert_graph(graph):
-    new_graph = {}
-    for node, children in graph.items():
-        # Criando a estrutura de nó esperada pela função iterative_deepening_dfs
-        new_graph[node] = {"value": node, "children": []}
-        for child in children:
-            new_graph[node]["children"].append({"value": child, "children": []})
-
-    # Conectando os nós filhos aos pais
-    for node, node_info in new_graph.items():
-        for child in node_info["children"]:
-            child.update(new_graph[child["value"]])
-
-    return new_graph
-
-converted_graph = convert_graph(graph)
-
-result = iterative_deepening_dfs(converted_graph[('A',)], ('M',))
-
-converted_graph = convert_graph(graph)
-solution, order_of_expansion, depth = iterative_deepening_dfs(converted_graph[('A',)], ('M',))
-
+solution, order_of_expansion, depth = iterative_deepening_dfs(graph, ('A',), ('M',))
 print(solution, order_of_expansion, depth)
