74dfa1031315d129a49440af6cba892a04a4b831
Beatriz238 <116269769+Beatriz238@users.noreply.github.com>
2023-12-30 16:32:50 +0000

    Classe Estafetas adicionada

---
+++ Estafetas.py
@@ -0,0 +1,31 @@
+import csv
+
+class Estafetas:
+    def __init__(self, Id, Ponto_Partida, Lista_Encomendas):
+        self.Id = Id
+        self.Ponto_Partida = Ponto_Partida 
+        self.Lista_Encomendas =Lista_Encomendas
+        
+    def __str__(self):
+        return f"ID: {self.Id}, Ponto de Partida: {self.Ponto_Partida}, Lista de Encomendas Atribuidas: {self.Lista_Encomendas}"
+    
+def ler_estafetas_csv(caminho_do_csv):
+    estafetas = []
+    with open(caminho_do_csv, 'r', encoding='utf-8') as arquivo_csv: #o encoding= 'utf-8' é para conseguir imprimir os caracteres com acento
+        leitor_csv = csv.reader(arquivo_csv, delimiter=';')
+
+        next(leitor_csv, None)
+        for linha in leitor_csv:
+            Id = int(linha[0])
+            Ponto_Partida = linha[1]
+            Lista_Encomendas = linha[2].split(',')  #na lista das encomendas, estas estão separadas por vírgulas
+
+            estafeta = Estafetas(Id, Ponto_Partida, Lista_Encomendas)
+            estafetas.append(estafeta)
+    return estafetas
+
+caminho_do_csv = 'Dataset\Estafetas.csv'
+
+estafetas = ler_estafetas_csv(caminho_do_csv)
+for estafeta in estafetas:
+    print(estafeta)
